82fb17854388709100524e44c9bb89f9d51592e5
iksuddle <90164642+iksuddle@users.noreply.github.com>
2024-06-11 00:23:57 +0400

    added frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+function onClickLogin() {
+
+}
+
+function App() {
+    return (
+        <>
+            <h1>RegexRank</h1>
+            <button onClick={onClickLogin}>
+                Login with GitHub
+            </button>
+        </>
+    )
+}
+
+export default App
